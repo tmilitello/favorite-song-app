51183e57aab2187773e45d92d361098e3ba16760
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-05 14:03:15 -0500

    adds all of project

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,4 +10,34 @@ class SongsController < ApplicationController
     render json: {song: song}
   end
 
+  def create
+    song = Song.create(
+      title: "sample_title",
+      album: "sample_album", 
+      artist: "sample_artist", 
+      year: "sample_year"
+    )
+    render json: {song: song}
+  end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.update(
+      title: "sample_title11",
+      album: "sample_album", 
+      artist: "sample_artist", 
+      year: "sample_year"
+    )
+    song.save
+    render json: {song: song}
+  end
+
+  def destroy
+    song = Song.find_by(id: params[:id])
+    song.delete
+
+    render json: {message: "This has been deleted"}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/songs', controller: "songs", action: "index"
   get '/songs/:id', controller: "songs", action: "show"
+  post '/songs', controller: "songs", action: "create"
+  patch '/songs/:id', controller: "songs", action: "update"
+  delete '/songs/:id', controller: "songs", action: "destroy"
 end
