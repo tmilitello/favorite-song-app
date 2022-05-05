1cb0052e0e2720e63c1bfba58ad79063b43c396d
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-05 12:47:57 -0500

    adds instances to seed file

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,14 @@
+class SongsController < ApplicationController
+
+  def index
+    
+  end
+
+
+
+
+
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Song.create(title: "Karma Police", album: "Okay Computer", artist: "Radiohead", year: "1997")
+
+Song.create(title: "Giving You Up", album: "Steady Heart", artist: "Kameron Marlowe", year: "2022")
+
+Song.create(title: "Star Spangled Banner", album: "Independence Songs", artist: "US Band", year: "1814")
